a36d4bb3fb096fffe058af53f03eddfcdb314c2a
dongwoo <>
2019-07-26 15:17:49 +0900

    add parameter store

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+import os
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Damon!'
+    name = os.environ.get('FLASK_NAME')
+    return f'Hello, {name if name is not None else "Damon"}!'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=80)
