91fc66e793b265776053b59711dbf9eb030e36ba
dongwoo <>
2019-07-31 14:35:40 +0900

    test

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     name = os.environ.get('FLASK_NAME')
-    return f'Hello, {name if name is not None else "Damon"}!'
+    return f'Hello, {name if name is not None else "Damon"}!!'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=80)
